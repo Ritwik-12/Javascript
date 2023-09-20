c7e41c366bdbe5eaec15e1e14a20336b18c0d60b
Ritwik <ritwikbhowmik659@gmail.com>
2023-09-20 17:20:14 +0530

    dtattypessummery

---
+++ datatypessummery.js
@@ -0,0 +1,54 @@
+//premitive and and non premitive datatypes or reference type
+
+//how data is stored in the memory and how it is accessed based
+//on that datatype is catagorized into tow parts
+
+
+//primitive
+
+//7 catagories
+//primitive types are call by value
+// 1.String
+// Number
+// Boolean
+// null(empty not zero)
+// undefined
+// Symbol//
+// BigInt
+
+//reference type (NON primitive)
+
+// Array
+// Objects
+// Function
+
+//js is a dynamically typed language not a staticaly typed language
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail;//by default it is undefinded
+
+const id=Symbol('123');
+const anoterId=Symbol('123')//it always take or  stores unique value      
+
+//non premetive data types return type is object
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
