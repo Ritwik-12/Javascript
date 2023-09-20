90519dabbe5837b059ff495a8f572875a8d6b71a
Ritwik <ritwikbhowmik659@gmail.com>
2023-09-20 16:56:10 +0530

    comparision of datatypes

--- Conversiondata.js
+++ Conversiondata.js
@@ -31,3 +31,19 @@ let sumNumber=33
 let stringnumber=String(sumNumber);
 console.log(stringnumber);
 console.log(typeof(stringnumber));
+
+//string concatination
+
+let str1="hello"
+let str2="Ritwik"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(true);
+console.log(+true)
