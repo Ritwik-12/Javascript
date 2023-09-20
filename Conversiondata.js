1d3258d5371a19f98488fdec5bef05e3adce8e54
Ritwik <ritwikbhowmik659@gmail.com>
2023-09-20 16:38:27 +0530

    conversion of datatypes

---
+++ Conversiondata.js
@@ -0,0 +1,33 @@
+let score=null
+console.log(typeof(score));
+
+console.log(typeof score);
+
+//convert string to number
+
+let valueinNumber=Number(score)
+
+console.log(typeof valueinNumber)
+
+console.log(valueinNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+
+//true =>1,false=>0
+
+let isLoggedIn="Ritwik"
+
+let booleanIslogedin=Boolean(isLoggedIn)
+console.log( booleanIslogedin)
+
+//1=>true ,0=>false
+
+//""=>false
+//"Ritwik"=>true
+
+let sumNumber=33
+
+let stringnumber=String(sumNumber);
+console.log(stringnumber);
+console.log(typeof(stringnumber));
